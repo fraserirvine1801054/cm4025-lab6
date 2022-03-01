23ce146f9b4eaa6ff1901ec2d2c0aeee8cfc91e9
Fraser Irvine <1801054@rgu.ac.uk>
2022-03-01 22:13:57 +0000

    edited app.listen on server.js

--- server/server.js
+++ server/server.js
@@ -2,22 +2,17 @@ import config from './../config/config';
 import app from './express';
 import mongoose from 'mongoose';
 
+//connection URL
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {dbName: "users"});
+mongoose.connection.on('error', err => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}` + err);
+});
+
 app.listen(config.port, (err) => {
+    console.info('Database is at %s.', config.mongoUri);
     if (err) {
         console.log(err);
     }
     console.info('Server started on port %s.', config.port);
 });
-
-//connection URL
-
-/**
- * make code read mongo URI from environment variables in AWS linux
- * 
- */
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.mongoUri, {dbName: "users"});
-mongoose.connection.on('error', err => {
-    throw new Error(`unable to connect to database: ${config.mongoUri}`);
-})
