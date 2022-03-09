71882a11266f7ddb4eee96346673ada3d10b8fba
Fraser Irvine <1801054@rgu.ac.uk>
2022-03-09 16:57:16 +0000

    completed part 4

--- server/express.js
+++ server/express.js
@@ -8,8 +8,22 @@ import helmet from 'helmet';
 import Template from './../template';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
+import devBundle from './devBundle';
+//modules for server side rendering
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import MainRouter from './../client/MainRouter';
+import {StaticRouter} from 'react-router-dom';
+import {ServerStyleSheets, ThemeProvider} from '@material-ui/styles';
+import theme from './../client/theme';
+//end
+
+const CURRENT_WORKING_DIR = process.cwd();
 
 const app = express();
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+devBundle.compile(app);
 
 //parse body params and attach them to the req.body
 
@@ -24,8 +38,29 @@ app.use(cors());
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
-app.get('/', (req,res) => {
-    res.status(200).send(Template());
+app.get('*', (req,res) => {
+    const sheets = new ServerStyleSheets();
+    const context = {};
+    const markup = ReactDOMServer.renderToString(
+        sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+                <ThemeProvider theme={theme}>
+                    <MainRouter/>
+                </ThemeProvider>
+            </StaticRouter>
+        )
+    );
+
+    if (context.url){
+        return res.redirect(303, context.url);
+    }
+
+    const css = sheets.toString();
+
+    res.status(200).send(Template({
+        markup: markup,
+        css: css
+    }));
 });
 
 app.use((err,req,res,next) => {
